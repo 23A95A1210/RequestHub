5c847f17570a1bed04c3f38ba5a745cf8043f4e4
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-05-21 11:30:57 +0530

    homebar

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RequestPortal from '../components/RequestHubNav'
+
+const Home = () => {
+  return (
+    <section>
+        <RequestPortal/>
+    </section>
+  )
+}
+
+export default Home
